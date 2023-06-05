b752d088348bc413e1c5343769c0a3e229c200da
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2023-06-05 22:46:37 +0300

    Fixed key error

--- src/Components/Playlists/Playlists.js
+++ src/Components/Playlists/Playlists.js
@@ -1,17 +1,15 @@
 //hooks
-import useFetscSpotifyData, {
+import useFetchSpotifyData, {
   SPOTIFY_DATA,
 } from "../../Hooks/useFetchSpotifyData";
 import { useStateProvider } from "../../Utils/StateProvider";
-//constants
-
 //styles
 import "./Playlists.css";
 
 const Playlists = () => {
   const { playlists } = useStateProvider();
 
-  useFetscSpotifyData(
+  useFetchSpotifyData(
     "https://api.spotify.com/v1/me/playlists?limit=20&offset=0",
     SPOTIFY_DATA.playlistsData
   );
@@ -20,16 +18,9 @@ const Playlists = () => {
     <div className="all-playlists">
       {playlists.map(({ name, id, images }) => {
         return (
-          <div className="single-playlist">
-            <img
-              src={images[0].url}
-              alt={name}
-              className="playlist-img"
-              key={id + 1}
-            />
-            <span key={id} className="playlist-name">
-              {name}
-            </span>
+          <div className="single-playlist" key={id}>
+            <img src={images[0].url} alt={name} className="playlist-img" />
+            <span className="playlist-name">{name}</span>
           </div>
         );
       })}
