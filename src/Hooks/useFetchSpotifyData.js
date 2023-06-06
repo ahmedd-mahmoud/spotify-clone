6192758d8d90c6df9d3a27323ac7c8169b65575c
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2023-06-06 07:56:15 +0300

    Added getPlaylistId function to custom hook

--- src/Hooks/useFetchSpotifyData.js
+++ src/Hooks/useFetchSpotifyData.js
@@ -5,13 +5,15 @@ import { reducerCases } from "../Utils/Reducer";
 export const SPOTIFY_DATA = {
   playlistsData: "playlistsData",
   userData: "userData",
+  selectedPlaylistData: "selectedPlaylistData",
+  selectedPlaylistId: "selectedPlaylistId",
 };
-const UseFetchSpotifyData = (url, spotifyData) => {
+const useFetchSpotifyData = (url, spotifyData) => {
   const { token, updateData } = useStateProvider();
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
 
-  const getPlaylistData = useCallback(
+  const getPlaylistsData = useCallback(
     (data) => {
       const { items } = data;
       const playlists = items.map(({ name, id, images }) => {
@@ -34,42 +36,91 @@ const UseFetchSpotifyData = (url, spotifyData) => {
     [updateData]
   );
 
-  useEffect(() => {
-    const getData = async () => {
-      setIsPending(true);
-      try {
-        const res = await fetch(url, {
-          method: "GET",
-          headers: {
-            Authorization: "Bearer " + token,
-            "Content-Type": "application/json",
-          },
-        });
-        if (!res.ok) {
-          throw new Error(res.statusText);
-        }
+  const getSelectedPlaylistId = useCallback(
+    (data) => {
+      const id = data.id;
 
-        const data = await res.json();
-        setIsPending(false);
-        setError(null);
-        if (spotifyData === SPOTIFY_DATA.playlistsData) {
-          getPlaylistData(data);
-        } else if (spotifyData === SPOTIFY_DATA.userData) {
-          getUserData(data);
-        }
-      } catch (err) {
-        if (err.name === "AbortError") {
-          console.log("The fetch was aborted");
-        } else {
-          setIsPending(false);
-          setError("Could not fetch the data");
-        }
+      updateData(id, reducerCases.SET_PLAYLIST_ID);
+    },
+    [updateData]
+  );
+
+  const getSelectedPlaylistData = useCallback(
+    (data) => {
+      const selectedPlaylist = {
+        playlistId: data.id,
+        playlistName: data.name,
+        playlistDescription: data.description.startsWith("<a")
+          ? ""
+          : data.description,
+        playlistImage: data.images[0].url,
+        playlistTracks: data.tracks.items.map(({ track, added_at }) => ({
+          trackId: track.id,
+          trackName: track.name,
+          trackDateAdded: added_at,
+          trackArtists: track.artists.map((artist) => artist.name),
+          trackImage: track.album.images[2].url,
+          trackDuration: track.duration_ms,
+          trackAlbum: track.album.name,
+          trackContext_uri: track.album.uri,
+          trackNumber: track.track_number,
+        })),
+      };
+
+      updateData(selectedPlaylist, reducerCases.SET_PLAYLIST);
+    },
+    [updateData]
+  );
+
+  const getData = useCallback(async () => {
+    setIsPending(true);
+    try {
+      const res = await fetch(url, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      });
+      if (!res.ok) {
+        throw new Error(res.statusText);
       }
-    };
+
+      const data = await res.json();
+      setIsPending(false);
+      setError(null);
+      if (spotifyData === SPOTIFY_DATA.playlistsData) {
+        getPlaylistsData(data);
+      } else if (spotifyData === SPOTIFY_DATA.userData) {
+        getUserData(data);
+      } else if (spotifyData === SPOTIFY_DATA.selectedPlaylistId) {
+        getSelectedPlaylistId(data);
+      } else if (spotifyData === SPOTIFY_DATA.selectedPlaylistData) {
+        getSelectedPlaylistData(data);
+      }
+    } catch (err) {
+      if (err.name === "AbortError") {
+        console.log("The fetch was aborted");
+      } else {
+        setIsPending(false);
+        setError("Could not fetch the data");
+      }
+    }
+  }, [
+    getPlaylistsData,
+    getUserData,
+    getSelectedPlaylistData,
+    getSelectedPlaylistId,
+    spotifyData,
+    token,
+    url,
+  ]);
+
+  useEffect(() => {
     getData();
-  }, [token, url, spotifyData, getPlaylistData, getUserData]);
+  }, [getData]);
 
   return { error, isPending };
 };
 
-export default UseFetchSpotifyData;
+export default useFetchSpotifyData;
