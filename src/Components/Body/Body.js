a03a51469e65b68032dc63348a144cda2e75bc23
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2023-06-06 07:58:23 +0300

    Fetched Playlist data and displayed it on screen

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,6 +1,6 @@
 import "./Body.css";
 
-import UseFetchSpotifyData, {
+import useFetchSpotifyData, {
   SPOTIFY_DATA,
 } from "../../Hooks/useFetchSpotifyData";
 import { HiOutlineUserGroup } from "react-icons/hi";
@@ -8,8 +8,19 @@ import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { useStateProvider } from "../../Utils/StateProvider";
 
 const Body = () => {
-  const { userInfo } = useStateProvider();
-  UseFetchSpotifyData("https://api.spotify.com/v1/me", SPOTIFY_DATA.userData);
+  const { userInfo, selectedPlaylist, selectedPlaylistId } = useStateProvider();
+  useFetchSpotifyData("https://api.spotify.com/v1/me", SPOTIFY_DATA.userData);
+
+  useFetchSpotifyData(
+    `https://api.spotify.com/v1/playlists/${selectedPlaylistId}`,
+    SPOTIFY_DATA.selectedPlaylistData
+  );
+
+  const msToMinutes = (ms) => {
+    const min = Math.floor(ms / 60000);
+    const sec = ((ms % 60000) / 1000).toFixed(0);
+    return min + ":" + (sec < 10 ? "0" : "") + sec;
+  };
 
   return (
     <div>
@@ -33,7 +44,115 @@ const Body = () => {
             </div>
           </div>
         </div>
-        <div className="songs">Songs</div>
+
+        {selectedPlaylist && (
+          <div className="playlist-content">
+            <div className="playlist-info">
+              <img
+                src={selectedPlaylist.playlistImage}
+                alt={selectedPlaylist.playlistName}
+                className="img"
+              />
+              <div className="playlist-details">
+                <span className="playlist-type">Playlist</span>
+                <h1 className="playlist-title">
+                  {selectedPlaylist.playlistName}
+                </h1>
+                <p className="description">
+                  {selectedPlaylist.playlistDescription}
+                </p>
+              </div>
+            </div>
+
+            <div className="list">
+              <div className="header">
+                <div className="number-title">
+                  <div className="header-col">
+                    <span>#</span>
+                  </div>
+                  <div className="header-col">
+                    <span>Title</span>
+                  </div>
+                </div>
+
+                <div className="header-col">
+                  <span>Album</span>
+                </div>
+                <div className="header-col">
+                  <span>Date Added</span>
+                </div>
+                <div className="header-col">
+                  <span>Duration</span>
+                </div>
+              </div>
+              <div className="tracks">
+                {selectedPlaylist.playlistTracks.map(
+                  (
+                    {
+                      trackId,
+                      trackName,
+                      trackDateAdded,
+                      trackArtists,
+                      trackImage,
+                      trackDuration,
+                      trackAlbum,
+                      trackContext_uri,
+                      trackNumber,
+                    },
+                    index
+                  ) => {
+                    return (
+                      <div className="row" key={trackId}>
+                        <div className="col-detail">
+                          <div className="col">
+                            <span>{index + 1}</span>
+                          </div>
+
+                          <img
+                            src={trackImage}
+                            alt={trackImage}
+                            className="track-img"
+                          />
+                          <div className="track-info">
+                            <span className="track-name">
+                              {trackName.length > 30
+                                ? trackName.slice(0, 25) + "..."
+                                : trackName}
+                            </span>
+                            <span className="track-artists">
+                              {trackArtists.length > 30
+                                ? trackArtists.slice(0, 25) + "..."
+                                : trackArtists}
+                            </span>
+                          </div>
+                        </div>
+                        <div className="col">
+                          <span className="track-album">
+                            {trackAlbum.length > 30
+                              ? trackAlbum.slice(0, 25) + "..."
+                              : trackAlbum}
+                          </span>
+                        </div>
+                        <div className="col">
+                          <span className="date-added">
+                            {trackDateAdded
+                              .slice(0, 10)
+                              .split("-")
+                              .reverse()
+                              .join("-")}
+                          </span>
+                        </div>
+                        <div className="col">
+                          <span>{msToMinutes(trackDuration)}</span>
+                        </div>
+                      </div>
+                    );
+                  }
+                )}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
